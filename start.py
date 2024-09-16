fa8a27e50a6d030e401666330aef9ecf1afb4bf3
RML-Cambo <ltomusic69@gmail.com>
2024-09-16 14:49:13 +0700

    Initilize

---
+++ start.py
@@ -0,0 +1,14 @@
+print("This is a new python")
+
+
+a =10
+print(a)
+b = "This is a start of python "
+print(a,b)
+
+a =10
+b =20
+if a > b :
+    print("A is bigger then B")
+else :
+    print("A is smaller then B")
