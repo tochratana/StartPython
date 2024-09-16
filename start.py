ed9fbb8e00ab990ff82814458b084346ce0c932a
RML-Cambo <ltomusic69@gmail.com>
2024-09-16 15:00:40 +0700

    Start

--- start.py
+++ start.py
@@ -5,10 +5,11 @@ a =10
 print(a)
 b = "This is a start of python "
 print(a,b)
-
+print("Hello")
 a =10
 b =20
 if a > b :
     print("A is bigger then B")
 else :
     print("A is smaller then B")
+print("This first git in pycharm")
